8fb09d50bcc642b007e3ac13d18cdce43046a34a
Von Ezekiel Manaois <von.manaois@gmail.com>
2023-12-06 19:13:06 -0500

    Dec 6 2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Form from './components/form/Form';
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+import LoginForm from './components/LoginForm/LoginForm';
 
 const App = () => {
+
+
     return (
-        <>
-            <Form />
-        </>
-    )
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/coverbuildergenerator" element={<Form />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
 }
 
 export default App
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ReactMarkDown from 'react-markdown';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBuildingUser, faUser, faWandMagicSparkles, faPeopleGroup, faLaptopFile, faFile} from '@fortawesome/free-solid-svg-icons'
+import './Form.css'
+import image1 from '../../assets/interview.svg'
+import image2 from '../../assets/generate.svg'
+
 const Form = () => {
 
   const [name, setName] = useState('');
@@ -8,7 +13,8 @@ const Form = () => {
   const [position, setPosition] = useState('');
   const [details, setDetails] = useState('');
   const [coverLetter, setCoverLetter] = useState('');
-
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   
   const [count, setCount] = useState(0);
@@ -28,26 +34,22 @@ const Form = () => {
     };
   }, [isLoading]);
 
-  const userMessages = [
-    { role: 'user', content: `Create a cover letter for ${name} with ${company} as ${position} and ${details} 
-    . You can use this format
-    
-    Dear Hiring Manager,
-      
-    
-         ...details here
-         
-    closing signature` },
-  ];
+  
   
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-    
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('company', company);
+    formData.append('position', position);
+    formData.append('details', details);
     axios
-      .post("http://localhost:8081/generate", { userMessages })
+      .post("http://localhost:8081/generate", { name, company, position, details})
       .then((response) => {
         setCoverLetter(response.data);
+        localStorage.setItem('coverLetter', coverLetter);
+        setCount(0)
       })
       .catch((error) => {
         console.error(error);
@@ -55,102 +57,126 @@ const Form = () => {
       .finally(() => {
         setIsLoading(false);
       });
+
       
+
+   const upload = new FormData();
+    upload.append("file", file);
+    await axios
+      .post("http://localhost:8081/upload", upload)
+      .then((res) => {})
+      .catch((er) => console.log(er));
   }
+        // //upload
+        //   const uploadData = () => {
+        //     const upload = new FormData();
+        //     upload.append("file", file);
+        //     axios
+        //       .post("http://localhost:8081/upload", upload)
+        //       .then((res) => {})
+        //       .catch((er) => console.log(er));
+        //   };
+       
+        
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+
+    if (selectedFile) {
+      setFile(selectedFile);
+      setFileName(selectedFile.name);
+    }
+  };
 
   return (
-    <div className="max-w-[80%] w-full mx-auto p-4 bg-[#99a8c3] justify-center mt-20">
-      <h1 className="text-3xl font-bold text-white flex justify-center mb-2">Cover Builder</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-      >
-        <div className="mb-4">
-          <label
-            for="name"
-            className="block text-gray-700 text-sm font-bold mb-2 uppercase"
-          >
-            Name
-          </label>
+    <div className="container-1">
+      <h1>Cover Builder</h1>
+      <div className="cover-container">
+        {/* <img src={image1} alt="" className="image-1" /> */}
+        <form onSubmit={handleSubmit} className="form-1">
+          <h3 className="page2-h3">We like to know you!</h3>
+          <div className="input-field-1">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name"
+              autoComplete="off"
+            />
+          </div>
+          <div className="input-field-1">
+            <FontAwesomeIcon icon={faBuildingUser} className="icon" />
+            <input
+              type="text"
+              id="company"
+              name="company"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+              placeholder="Company"
+              autoComplete="off"
+            />
+          </div>
+          <div className="input-field-1">
+            <FontAwesomeIcon icon={faPeopleGroup} className="icon" />
+            <input
+              type="text"
+              id="position"
+              name="position"
+              value={position}
+              onChange={(e) => setPosition(e.target.value)}
+              placeholder="Position"
+              autoComplete="off"
+            />
+          </div>
+          <div className="input-field-text">
+            <FontAwesomeIcon icon={faLaptopFile} className="icon" />
+            <textarea
+              id="details"
+              name="details"
+              rows="3"
+              value={details}
+              onChange={(e) => setDetails(e.target.value)}
+              placeholder="Paste Job Description Here..."
+            ></textarea>
+          </div>
+          {/* <h4 className='resume-h4'>Attach Resume</h4> */}
+          {/* <div className="input-field-1"> */}
+          <FontAwesomeIcon icon={faFile} className="icon" />
           <input
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter your name here"
+            type="file"
+            name="resume"
+            id="resume"
+            className="upload" 
+            hidden           
+            onChange={handleFileChange}
           />
-        </div>
-        <div className="mb-6">
-          <label
-            for="company"
-            className="block text-gray-700 text-sm font-bold mb-2 uppercase"
-          >
-            Company
+          <label htmlFor="resume" className="label-resume">
+            Attach Resume
           </label>
-          <input
-            type="text"
-            id="company"
-            name="company"
-            value={company}
-            onChange={(e) => setCompany(e.target.value)}
-            required
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-            placeholder="Enter the company you want to apply"
-          />
-        </div>
-        <div className="mb-6">
-        <label
-          for="position"
-          className="block text-gray-700 text-sm font-bold mb-2 uppercase"
-        >
-          Position
-        </label>
-        <input
-          type="text"
-          id="position"
-          name="position"
-          value={position}
-          onChange={(e) => setPosition(e.target.value)}
-          required
-          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-          placeholder='Enter the position you want to apply'
-        />
-        </div>
-        <div className="mb-10">
-        <label
-          for="others"
-          className="block text-gray-700 text-sm font-bold mb-2 uppercase"
-        >
-          Brief details about yourself
-        </label>
-        <textarea
-          id="others"
-          name="others"
-          rows="3"
-          value={details}
-          onChange={(e) => setDetails(e.target.value)}
-          placeholder='Enter any other information you want to add'
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        ></textarea>
-        </div>
-        <div className="mb-6">
-        <button
-          type="submit"
-          className="block  bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Generate Cover Letter
-        </button>
-        </div>
-      </form>
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8">
-        <h2 className="text-2xl font-bold mb-4 text-gray-800">Generated Cover Letter</h2>
-        <div className="mb-4 border-2 shadow rounded px-10 py-10 bg-white">
-        {isLoading && <div className="p-4">Please wait while we generate your cover letter...{count}</div>}
-        <ReactMarkDown className="text-[16px] text-gray-800">{coverLetter}</ReactMarkDown>
-        </div>
+          <span className='file-name'>{fileName}</span>
+          {/* </div> */}
+
+          <button type="submit" className="btn2">
+            Generate Cover Letter
+          </button>
+
+          {/* <img src={image2} alt='image2' className='image2' /> */}
+        </form>
+        {/* <button type="submit" className="btn2">
+              Upload
+            </button> */}
+        <form className="form-2">
+          {/* <h3 className='page2-h3'>Cover Letter</h3> */}
+          {isLoading && (
+            <div className="loading">
+              <FontAwesomeIcon icon={faWandMagicSparkles} />
+              Please wait while we generate your cover letter...{count}
+            </div>
+          )}
+          <textarea className="result-letter" value={coverLetter} disabled />
+        </form>
       </div>
     </div>
   );
